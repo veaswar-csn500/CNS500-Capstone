271caeeb1849309e94c872eccd1f7e8476a9665a
veaswar-csn500 <veaswar@myseneca.ca>
2022-07-12 13:09:12 -0400

    Adds VMDeploy.bash file

---
+++ IND6/VMDeploy.bash
@@ -0,0 +1,4 @@
+az deployment group create --resource-group Student-RG-XXXXXX --template-file ./WC-48.json --no-wait
+az deployment group create --resource-group Student-RG-XXXXXX --template-file ./WS-48.json --no-wait
+az deployment group create --resource-group Student-RG-XXXXXX --template-file ./LR-48.json --no-wait
+az deployment group create --resource-group Student-RG-XXXXXX --template-file ./LS-48.json --no-wait
