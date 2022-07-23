98904b258b78169736d73265d3dabb69f34f8022
veaswar-csn500 <veaswar@myseneca.ca>
2022-07-22 22:20:00 -0400

    adds devtest_lab_create.sh

---
+++ IND7/devtest_lab_create.sh
@@ -0,0 +1,11 @@
+# Creating Checkpoint1 Resources with bash
+
+# echo "Load variables from config.sh"
+source ./config.sh
+
+######################################
+#Devtest Lab Deployment
+echo "----------------------"
+echo "Running Next ---------"
+echo "Creating Routes"
+# --> Your Code in here
