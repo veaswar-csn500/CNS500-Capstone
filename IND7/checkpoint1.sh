a1f7977d5497863e054b8ad973e74e034de11762
veaswar-csn500 <veaswar@myseneca.ca>
2022-07-22 22:07:51 -0400

    adds checkpoint.sh

---
+++ IND7/checkpoint1.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/bash
+
+# Creating Checkpoint1 Resources with bash
+echo "--------------------------------"
+echo "Loadign the variables"
+source ./config.sh
+echo "Student Deafault Resource Group $RG_Name"
+echo "--------------------------------"
+echo 
+
+echo "--------------------------------"
+echo "Creating VNETs"
+echo "--------------------------------"
+echo 
+source ./vnet_create.sh
+
+echo "--------------------------------"
+echo "Creating Route Tables"
+echo "--------------------------------"
+echo 
+source ./rout_tbl_create.sh
