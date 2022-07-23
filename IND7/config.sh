2f46b077aa11f2af498cff03a39fb13735cbb8ad
veaswar-csn500 <veaswar@myseneca.ca>
2022-07-22 21:42:56 -0400

    updates config.sh file

---
+++ IND7/config.sh
@@ -0,0 +1,19 @@
+# Creating Checkpoint2 Resources with bash
+
+# Variable block
+Location="Canada East"
+RG_Name="Student-RG-634439"
+Student_vnet="Student-634439-vnet"
+Router_vnet="Router-48"
+Server_vnet="Server-48"
+SubNet="SN1"
+RT_Name="RT-48"
+Client_SN="Virtual-Desktop-Client"
+Virtual_Appliance="192.168.48.36"
+Server_SN1="172.17.48.32/27"
+Virtual_Desktop="10.22.122.0/24"
+DevTest_Lab="CSN500-48"
+VM_Win_Client="WC-48"
+VM_Win_Server="WS-48"
+VM_Lnx_Router="LR-48"
+VM_Lnx_Server="LS-48"
